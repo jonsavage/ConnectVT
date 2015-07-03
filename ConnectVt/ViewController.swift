7e326a71bcf4f7ef97f2eb7f1cd650d740f9ab44
Jonathan Savage <jon5@vt.edu>
2015-07-03 19:30:48 -0400

    Initial Commit

---
+++ ConnectVt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectVt
+//
+//  Created by Jon Savage on 7/3/15.
+//  Copyright © 2015 Jon Savage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
